176810784488a78b08bb903939f606a2e6e5a005
Noah Naime <dr.no.name@web.de>
2014-10-21 22:33:56 +0200

    Test for backpropagation

--- src/aNN_Tests.java
+++ src/aNN_Tests.java
@@ -14,88 +14,18 @@ public class aNN_Tests {
 		
 		//uTNeuron.runTest();
 		
-		// Test for whole network, needs debug prints, look at commit when passed,
-		// in class otherwise you'll just see the output vector
-		multilayerPerceptronTest multilayerPerceptronTest = new multilayerPerceptronTest();
+		// Test for whole network, needs debug prints, look at commit when passed, in class
+		// otherwise you'll just see the output vector
+		//multilayerPerceptronTest multilayerPerceptronTest = new multilayerPerceptronTest();
 		
-		multilayerPerceptronTest.runTest();
+		//multilayerPerceptronTest.runTest();
 		
-		//TODO: Test mit Training: Backpropagation
-		//		-> Vergleich von verschiedenen Topologien, welche erreicht am schnellsten die lösung
+		// Test for training using backpropagation
+		// TODO: -> Vergleich von verschiedenen Topologien, welche erreicht am schnellsten die lösung
+		backpropagationTest backpropagationTest = new backpropagationTest();
+		
+		backpropagationTest.runTest();
 	}
-}
-
-/******************************** Test for multilayer perceptron ********************************
-* In a loop:
-*	-> inputs are set and unset
-*	-> output of input neurons is calculated -> in connection
-*	------------------------------------------
-*	-> input of hidden neurons is set or unset -> in connection
-*	-> weight of hidden neuron is set (output value * connection weight) -> in connection
-*	-> output of hidden neurons is calculated
-*	------------------------------------------
-*	-> input of output neuron is set or unset
-*	-> weight of output neuron is set 
-*	-> output of output neurons is calculated
-*
-*	-> Online-learning: Calculate weights and threshold for first result, than second etc
-*	-> Batch-learning: Calculate all results, then weights and thresholds
-************************************************************************************************/
-class multilayerPerceptronTest {
-private
-	MultiLayerPerceptron MultiLayerPerceptron[];
-	
-public	
-	// Constructor
-	multilayerPerceptronTest(){
-		//MultiLayerPerceptron(inputNeurons, hiddenNeuronsPerLayer, hiddenLayers, outputNeurons)
-		// TODO: Needed Tests:	// From each neuron to each neuron
-								// Split input neurons in 2 groups
-								// Split hidden neurons in 2 groups
-								// From each neuron to each neuron (between hidden layers)
-								// connectionsPerNeuron < hiddenNeuron.length
-								// Test für Teilerreste
-	
-		MultiLayerPerceptron = new MultiLayerPerceptron[3];
-		MultiLayerPerceptron[0] = new MultiLayerPerceptron(2, 1, 1, 1);
-		MultiLayerPerceptron[1] = new MultiLayerPerceptron(4, 2, 2, 2);
-		MultiLayerPerceptron[2] = new MultiLayerPerceptron(8, 12, 6, 4);
-	}
-	
-	/***************************************************************************************
-	* Test function:
-	***************************************************************************************/
-	void runTest(){
-		// Switch the inputs on and off (all possibilities)
-		//for (int inputVector = 0; inputVector < 16; inputVector++){
-		//	float[] testResult = mLPTest.run(inputValues);
-		
-		float[] inputVector;
-		inputVector = new float[8];
-		
-		for (int i = 0; i < inputVector.length; i++){
-			inputVector[i] = 1;
-		}
-		
-		for (int i = 0; i < MultiLayerPerceptron.length; i++){
-			
-			// Run with all inputs high
-			float[] outputVector = MultiLayerPerceptron[i].run(inputVector);
-			
-			for (int nrVec = 0; nrVec < outputVector.length; nrVec++){
-				if (nrVec > 0)
-					System.out.print(", ");
-				
-				System.out.print("outputVector[" + nrVec + "]: " + outputVector[nrVec]);
-			}
-			
-			System.out.print("\n--------------------------------------------------------\n");
-			
-			//TODO: run test again with int input vector
-			//int outputVector = MultiLayerPerceptron[i].runInt(0b11111111);
-		}
-		//}
-	}// runTest()
 }
 
 /*************************************** Unit Test Neuron ***************************************
@@ -248,4 +178,163 @@ public
 		
 		System.out.print("\n");
 	}
-}
+}// class unitTestNeuron
+
+/******************************** Test for multilayer perceptron ********************************
+* In a loop:
+*	-> inputs are set and unset
+*	-> output of input neurons is calculated -> in connection
+*	------------------------------------------
+*	-> input of hidden neurons is set or unset -> in connection
+*	-> weight of hidden neuron is set (output value * connection weight) -> in connection
+*	-> output of hidden neurons is calculated
+*	------------------------------------------
+*	-> input of output neuron is set or unset
+*	-> weight of output neuron is set 
+*	-> output of output neurons is calculated
+*
+*	-> Online-learning: Calculate weights and threshold for first result, than second etc
+*	-> Batch-learning: Calculate all results, then weights and thresholds
+************************************************************************************************/
+class multilayerPerceptronTest {
+private
+	MultiLayerPerceptron MultiLayerPerceptron[];
+	
+public	
+	// Constructor
+	multilayerPerceptronTest(){
+		//MultiLayerPerceptron(inputNeurons, hiddenNeuronsPerLayer, hiddenLayers, outputNeurons)
+		// TODO: Needed Tests:	// From each neuron to each neuron
+								// Split input neurons in 2 groups
+								// Split hidden neurons in 2 groups
+								// From each neuron to each neuron (between hidden layers)
+								// connectionsPerNeuron < hiddenNeuron.length
+								// Test für Teilerreste
+	
+		MultiLayerPerceptron = new MultiLayerPerceptron[2];
+		MultiLayerPerceptron[0] = new MultiLayerPerceptron(2, 1, 1, 1);
+		MultiLayerPerceptron[1] = new MultiLayerPerceptron(4, 2, 2, 2);
+		//MultiLayerPerceptron[2] = new MultiLayerPerceptron(8, 12, 6, 4);
+	}
+	
+	/***************************************************************************************
+	* Test function:
+	***************************************************************************************/
+	void runTest(){
+		// Switch the inputs on and off (all possibilities)
+		//for (int inputVector = 0; inputVector < 16; inputVector++){
+		//	float[] testResult = mLPTest.run(inputValues);
+		
+		float[] inputVector;
+		inputVector = new float[8];
+		
+		for (int i = 0; i < inputVector.length; i++){
+			inputVector[i] = 1;
+		}
+		
+		for (int i = 0; i < MultiLayerPerceptron.length; i++){
+			
+			// Run with all inputs high
+			float[] outputVector = MultiLayerPerceptron[i].run(inputVector);
+			
+			for (int nrVec = 0; nrVec < outputVector.length; nrVec++){
+				if (nrVec > 0)
+					System.out.print(", ");
+				
+				System.out.print("outputVector[" + nrVec + "]: " + outputVector[nrVec]);
+			}
+			
+			System.out.print("\n--------------------------------------------------------\n");
+			
+			//TODO: run test again with int input vector
+			//int outputVector = MultiLayerPerceptron[i].runInt(0b11111111);
+		}
+		//}
+	}// runTest()
+}// class multilayerPerceptronTest
+
+/****************************** Test for backpropagation training *******************************
+* Builts a multilayer perceptron and executes the training function
+************************************************************************************************/
+class backpropagationTest {
+private
+	MultiLayerPerceptron  perceptron;
+	float[] trainingInVector;
+	float[] trainingOutVector;
+	int numberInputs;
+	int numberOutputs;
+	int numberHiddenNeurons;
+	int numberHiddenLayers;
+
+public
+	// Constructor
+	backpropagationTest(){
+		numberInputs = 4;
+		numberOutputs = 2;
+		numberHiddenNeurons = 2;
+		numberHiddenLayers = 1;
+		
+		trainingInVector = new float[numberInputs];
+		trainingOutVector = new float[numberOutputs];
+		
+		perceptron = new MultiLayerPerceptron(numberInputs, numberHiddenNeurons, numberHiddenLayers, numberOutputs);
+	}
+
+	void runTest(){
+		if(numberOutputs > numberHiddenNeurons)
+			System.out.println("numberOutputs ("+numberOutputs+") > numberHiddenNeurons ("+numberHiddenNeurons+")");
+		else {
+//			System.out.println(
+//					"-------------------------------- 1st run --------------------------------"
+//					);
+			
+			trainingInVector[0]= 1;
+			trainingInVector[1]= 1;
+			trainingInVector[2]= 0;
+			trainingInVector[3]= 0;
+//			trainingInVector[4]= 1;
+//			trainingInVector[5]= 0.5f;
+//			trainingInVector[6]= 0.5f;
+//			trainingInVector[7]= 1;
+			
+			trainingOutVector[0] = 0.8f;
+			trainingOutVector[1] = 0.5f;
+			
+			perceptron.training(trainingInVector, trainingOutVector);
+/*			
+			System.out.println(
+					"-------------------------------- 2nd run --------------------------------"
+					);
+			
+			trainingInVector[0]= 0.5f;
+			trainingInVector[1]= 0.5f;
+			trainingInVector[2]= 1;
+			trainingInVector[3]= 1;
+			trainingInVector[4]= 0.5f;
+			trainingInVector[5]= 0;
+			trainingInVector[6]= 0;
+			trainingInVector[7]= 0.5f;
+			
+			trainingOutVector[0] = 0.2f;
+			
+			perceptron.training(trainingInVector, trainingOutVector);
+
+			System.out.println(
+					"-------------------------- Run with 1st input ---------------------------"
+					);
+			trainingInVector[0]= 1;
+			trainingInVector[1]= 1;
+			trainingInVector[2]= 0;
+			trainingInVector[3]= 0;
+			trainingInVector[4]= 1;
+			trainingInVector[5]= 0.5f;
+			trainingInVector[6]= 0.5f;
+			trainingInVector[7]= 1;
+			
+			float out[] = perceptron.run(trainingInVector);
+			
+			System.out.println("out[0]: "+out[0]);
+*/		}
+	}
+
+}// class backpropagationTest
